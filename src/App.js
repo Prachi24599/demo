c7f602ced2d07d330eea57e8f7ddaf74474609f3
Prachi24599 <prachi24599@gmail.com>
2023-06-03 01:01:59 +0530

    Passing Props value using custom attribute

--- src/App.js
+++ src/App.js
@@ -4,15 +4,47 @@ import Item from "./components/Item";
 import ItemDate from "./components/ItemDate";
 
 function App() {
-  const itemTwoName = "SurfExcel";
+  const response = [
+    {
+      itenName: "Nirma",
+      itemDay: 24,
+      itemMonth: "May",
+      itemYear: "1999",
+    },
+    {
+      itenName: "SurfExcel",
+      itemDay: 14,
+      itemMonth: "March",
+      itemYear: "1995",
+    },
+    {
+      itenName: "Ghadi",
+      itemDay: 9,
+      itemMonth: "Dec",
+      itemYear: "2004",
+    },
+  ];
   return (
     <div>
-      <Item name="RIN"></Item>
-      <ItemDate day="24" month="May" year="1999"></ItemDate>
-      <Item name={itemTwoName}></Item>
-      <ItemDate day="10" month="Jan" year="2002"></ItemDate>
-      <Item name="Ghadi"></Item>
-      <ItemDate day="31" month="March" year="1995"></ItemDate>
+      <Item name={response[0].itenName}></Item>
+      <ItemDate
+        day={response[0].itemDay}
+        month={response[0].itemMonth}
+        year={response[0].itemYear}
+      ></ItemDate>
+      <Item name={response[1].itenName}></Item>
+      <ItemDate
+        day={response[1].itemDay}
+        month={response[1].itemMonth}
+        year={response[1].itemYear}
+      ></ItemDate>
+      <Item name={response[2].itenName}></Item>
+      <ItemDate
+        day={response[2].itemDay}
+        month={response[2].itemMonth}
+        year={response[2].itemYear}
+      ></ItemDate>
+
       <div className="App">Hello Ji</div>
     </div>
   );
